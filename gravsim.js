a1c28073e91eeb8ec66afb1eb2cf23ba1ba8affc
steelmaker86 <Steelmaker86@gmail.com>
2022-05-11 15:13:51 -0500

    Update gravsim.js

--- gravsim.js
+++ gravsim.js
@@ -1,13 +1,13 @@
-const tableEle = document.createElement("table");
-console.log(tableEle)
+const tableEl = document.createElement("table");
+console.log(tableEl)
 for (var i=0;i<=490;i++) {
   let tableRow = document.createElement("tr");
   tableRow.className = `row${i}`;
-  tableEle.appendChild(tableRow);
+  tableEl.appendChild(tableRow);
   for (var k=0;k<=490;k++) {
     let tableEntry = document.createElement("td");
     tableEntry.classname = `col${k}`;
     tableRow.appendChild(tableEntry);
   }
 }
-document.body.appendChild(tableEle);
+document.body.appendChild(tableEl);

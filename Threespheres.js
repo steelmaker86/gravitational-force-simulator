31c25f27277dfe52c559975f58d39d079c3e8649
steelmaker86 <Steelmaker86@gmail.com>
2022-05-24 12:23:10 -0500

    Update Threespheres.js

--- Threespheres.js
+++ Threespheres.js
@@ -24,16 +24,8 @@ const material2 = new THREE.MeshBasicMaterial({
 });
 const sphere2 = new THREE.Mesh(geometry, material);
 scene.add(sphere2);
-const geometry3 = new THREE.SphereGeometry(1, 10, 10);
-const material3 = new THREE.MeshBasicMaterial({
-  color: 0xffff00,
-  wireframe: true,
-});
-const sphere3 = new THREE.Mesh(geometry, material);
-scene.add(sphere3);
-sphere.position.x = -1;
+sphere.position.x = -2;
 sphere2.position.x = 2;
-sphere3.position.x = -5;
 camera.position.z = 10;
 var zoomstate = 0;
 var angleY = 0;
@@ -42,20 +34,16 @@ var rotatestate = 0;
 var yrotatestate = 0;
 var sphereVector1 = new THREE.Vector3( 0, 0, 0 );
 var sphereVector2 = new THREE.Vector3( 0, 0, 0 );
-var sphereVector3 = new THREE.Vector3( 0, 0, 0 );
+
 var gForce = 0;
-var Vel1 = 0;
+var Vel1 = 0.01;
+var Vel2 = 0;
+var VelV1 = new THREE.Vector3( 0, 0, 10 );
+var VelV2 = new THREE.Vector3( 0, 0, -10 );
 var dist1 = 0;
 var dist2 = 0;
 function animate() {
   requestAnimationFrame(animate);
-  /*  sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.01;
-  sphere2.rotation.x += 0.01;
-  sphere2.rotation.y += 0.01;
-	sphere3.rotation.x += 0.01;
-	sphere3.rotation.y += 0.01;
-*/
   renderer.render(scene, camera);
   /*camera.rotation.y = (90 * Math.PI) / 180 - angleY;*/
 
@@ -98,42 +86,24 @@ function animate() {
   sphereVector2.x = sphere2.position.x
   sphereVector2.y = sphere2.position.y
   sphereVector2.z = sphere2.position.z
-  sphereVector3.z = sphere3.position.z
-  sphereVector3.z = sphere3.position.x
-  sphereVector3.z = sphere3.position.y
   gForce = (1/sphereVector1.distanceToSquared(sphereVector2))*(6.67430e-6);
+  VelV1.x = (VelV1.x + (gForce/VelV1.distanceTo(sphereVector2)) * (sphereVector2.x-VelV1.x))
+  VelV1.y = (VelV1.y + (gForce/VelV1.distanceTo(sphereVector2)) * (sphereVector2.y-VelV1.y))
+  VelV1.z = (VelV1.z + (gForce/VelV1.distanceTo(sphereVector2)) * (sphereVector2.z-VelV1.z))
+  VelV2.x = (VelV2.x + (gForce/VelV2.distanceTo(sphereVector1)) * (sphereVector1.x-VelV2.x))
+  VelV2.y = (VelV2.y + (gForce/VelV2.distanceTo(sphereVector1)) * (sphereVector1.y-VelV2.y))
+  VelV2.z = (VelV2.z + (gForce/VelV2.distanceTo(sphereVector1)) * (sphereVector1.z-VelV2.z))
 // calculates the gravitational force/acceleration between the two spheres
-  Vel1 += gForce;
+  Vel1 -= gForce;
   dist1 = sphereVector1.distanceTo(sphereVector2)
-  sphere.position.x = (sphereVector1.x + (Vel1/sphereVector1.distanceTo(sphereVector2)) * (sphereVector2.x-sphereVector1.x))
-  sphere.position.y = (sphereVector1.y + (Vel1/sphereVector1.distanceTo(sphereVector2)) * (sphereVector2.y-sphereVector1.y))
-  sphere.position.z = (sphereVector1.z + (Vel1/sphereVector1.distanceTo(sphereVector2)) * (sphereVector2.z-sphereVector1.z))
-  sphere2.position.x = (sphereVector2.x + (Vel1/sphereVector1.distanceTo(sphereVector2)) * (sphereVector1.x-sphereVector2.x))
-  sphere2.position.y = (sphereVector2.y + (Vel1/sphereVector1.distanceTo(sphereVector2)) * (sphereVector1.y-sphereVector2.y))
-  sphere2.position.z = (sphereVector2.z + (Vel1/sphereVector1.distanceTo(sphereVector2)) * (sphereVector1.z-sphereVector2.z))
-  sphereVector1.x = sphere.position.x
-  sphereVector1.y = sphere.position.y
-  sphereVector1.z = sphere.position.z
-  sphereVector2.x = sphere2.position.x
-  sphereVector2.y = sphere2.position.y
-  sphereVector2.z = sphere2.position.z
-  sphereVector3.z = sphere3.position.z
-  sphereVector3.z = sphere3.position.x
-  sphereVector3.z = sphere3.position.y
+  sphere.position.x = (sphereVector1.x + (Vel1/sphereVector1.distanceTo(VelV1)) * (VelV1.x-sphereVector1.x))
+  sphere.position.y = (sphereVector1.y + (Vel1/sphereVector1.distanceTo(VelV1)) * (VelV1.y-sphereVector1.y))
+  sphere.position.z = (sphereVector1.z + (Vel1/sphereVector1.distanceTo(VelV1)) * (VelV1.z-sphereVector1.z))
+  sphere2.position.x = (sphereVector2.x + (Vel1/sphereVector1.distanceTo(VelV1)) * (VelV2.x-sphereVector2.x))
+  sphere2.position.y = (sphereVector2.y + (Vel1/sphereVector1.distanceTo(VelV1)) * (VelV2.y-sphereVector2.y))
+  sphere2.position.z = (sphereVector2.z + (Vel1/sphereVector1.distanceTo(VelV1)) * (VelV2.z-sphereVector2.z))
   dist2 = sphereVector1.distanceTo(sphereVector2)
   console.log(`${(dist1-dist2)} ${2*Vel1}`);
-  /*sphere.position.x = (sphereVector1.x + Vel1 * (sphereVector3.x-sphereVector1.x))
-  sphere.position.y = (sphereVector1.y + Vel1 * (sphereVector3.y-sphereVector1.y))
-  sphere.position.z = (sphereVector1.z + Vel1 * (sphereVector3.z-sphereVector1.z))
-  sphere2.position.x = (sphereVector2.x + Vel1 * (sphereVector3.x-sphereVector2.x))
-  sphere2.position.y = (sphereVector2.y + Vel1 * (sphereVector3.y-sphereVector2.y))
-  sphere2.position.z = (sphereVector2.z + Vel1 * (sphereVector3.z-sphereVector2.z))
-  sphere3.position.x = (sphereVector3.x + Vel1 * (sphereVector1.x-sphereVector3.x))
-  sphere3.position.y = (sphereVector3.y + Vel1 * (sphereVector1.y-sphereVector3.y))
-  sphere3.position.z = (sphereVector3.z + Vel1 * (sphereVector1.z-sphereVector3.z))
-  sphere3.position.x = (sphereVector3.x + Vel1 * (sphereVector2.x-sphereVector3.x))
-  sphere3.position.y = (sphereVector3.y + Vel1 * (sphereVector2.y-sphereVector3.y))
-  sphere3.position.z = (sphereVector3.z + Vel1 * (sphereVector2.z-sphereVector3.z)) */
   camera.lookAt(0,0,0);
 
 }
